767d0aa0c0145321555cae355d88e4f37fdc98b1
Kamil Szerląg <kamilszerlag@gmail.com>
2021-05-31 17:48:17 +0200

    create-form

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
+import Form from 'features/Form/Form';
 
-const Root = () => (
-  <div className="App">
-    <h1>Hello!</h1>
-  </div>
-);
+const Root = () => {
+  const showValues = (values) => {
+    console.log(values);
+  };
+  return (
 
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        <Form formValues={showValues} />
+      </div>
+    </ThemeProvider>
+  );
+};
 export default Root;
